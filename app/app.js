4d81b5ddf569c4d30c26a46934c377c49f7823da
Viier <caioviier@gmail.com>
2022-07-14 20:32:41 -0300

    chore: adaptação do app para testes

--- app/app.js
+++ app/app.js
@@ -5,14 +5,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use(routes.creditCards);
-// app.use(routes.payments);
-// app.use(routes.receipts);
-// app.use(routes.reports);
+app.use(routes.creditCards);
+app.use(routes.payments);
+app.use(routes.receipts);
+app.use(routes.reports);
 
 app.listen(config.app.port, () => {
 	console.log("Server rodando")
